dce3713128ea953be8ab9d00d5543695203549eb
MrPancakes39 <salmannhassan39@gmail.com>
2022-11-14 18:54:27 +0200

    Renamed data folder to example

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod merge;
 use merge::{merge_files, MergeError};
 
 fn main() -> Result<(), MergeError> {
-    let list = vec!["src/data/notebook1.xopp", "src/data/notebook2.xopp"];
-    merge_files(list, "src/data/output.xopp").unwrap();
+    let list = vec!["src/example/notebook1.xopp", "src/example/notebook2.xopp"];
+    merge_files(list, "src/example/output.xopp").unwrap();
     Ok(())
 }
