92e02b2fdb463e32ad18e9101a803f88933c4d3d
MrPancakes39 <salmannhassan39@gmail.com>
2022-11-14 23:19:57 +0200

    Removed unnecessary Result in main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 mod cli;
 mod merge;
 
-use anyhow::Result;
 use cli::*;
 use merge::merge_files;
 use std::{env, process};
 
-fn main() -> Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
     let (input, output) = parse_args(&args).unwrap_or_else(|err| {
         handle_error(err);
         process::exit(1);
     });
     merge_files(input, &output).unwrap();
-    Ok(())
 }
