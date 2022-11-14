3a71d91ea9eda53a23f11117767cf0d79c4ef7f6
MrPancakes39 <salmannhassan39@gmail.com>
2022-11-14 23:13:48 +0200

    Moved parse_args to its own module

---
+++ src/cli.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+pub enum ParseError {
+    NeedHelp,
+    NotEnoughArgs,
+}
+
+pub fn parse_args(args: &[String]) -> Result<(&[String], &String), ParseError> {
+    // command help
+    if args.len() == 2 && args[1] == "help" {
+        return Err(ParseError::NeedHelp);
+    }
+    // min: command input1 input2 output
+    if args.len() < 4 {
+        return Err(ParseError::NotEnoughArgs);
+    }
+
+    // get input and output
+    let n = args.len();
+    let input = &args[1..n - 1];
+    let output = &args[n - 1];
+
+    Ok((input, output))
+}
+
+pub fn handle_error(err: ParseError) {}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,17 @@
+mod cli;
 mod merge;
 
 use anyhow::Result;
+use cli::*;
 use merge::merge_files;
-use std::env;
+use std::{env, process};
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    let (input, output) = parse_args(&args).unwrap();
-    println!("input: {:?}", input);
-    println!("output: {:?}", output);
+    let (input, output) = parse_args(&args).unwrap_or_else(|err| {
+        handle_error(err);
+        process::exit(1);
+    });
     merge_files(input, &output).unwrap();
     Ok(())
-}
-
-#[derive(Debug)]
-enum ParseError {
-    NeedHelp,
-    NotEnoughArgs,
-}
-
-fn parse_args(args: &[String]) -> Result<(&[String], &String), ParseError> {
-    // command help
-    if args.len() == 2 && args[1] == "help" {
-        return Err(ParseError::NeedHelp);
-    }
-    // min: command input1 input2 output
-    if args.len() < 4 {
-        return Err(ParseError::NotEnoughArgs);
-    }
-
-    // get input and output
-    let n = args.len();
-    let input = &args[1..n - 1];
-    let output = &args[n - 1];
-
-    Ok((input, output))
 }
